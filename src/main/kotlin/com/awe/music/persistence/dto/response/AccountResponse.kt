b19927c1a95180cde9210f4e0cdfe9d0545f0a75
HolkerDev <nick.penkovskiy@gmail.com>
2020-07-10 18:34:21 +0200

    Fixed parsing problems

--- src/main/kotlin/com/awe/music/persistence/dto/response/AccountResponse.kt
+++ src/main/kotlin/com/awe/music/persistence/dto/response/AccountResponse.kt
@@ -1,14 +1,14 @@
 package com.awe.music.persistence.dto.response
 
 import com.awe.music.persistence.domain.Account
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * Class represents response object for create a new account action.
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
-class AccountResponse constructor(account: Account) {
+
+class AccountResponse @JsonIgnore constructor(account: Account) {
     @field:JsonProperty("username")
     val username = account.username
 
@@ -18,7 +18,7 @@ class AccountResponse constructor(account: Account) {
     @field:JsonProperty("full_name")
     val name = account.name
 
-    @field:JsonProperty("is_collective")
+    @get:JsonProperty("is_collective")
     val isCollective = account.isCollective
 
     @field:JsonProperty("uuid")
@@ -27,13 +27,13 @@ class AccountResponse constructor(account: Account) {
     @field:JsonProperty("followers")
     val followers = account.followerCounter
 
-    @field:JsonProperty("is_activated")
+    @get:JsonProperty("is_activated")
     val isActivated = account.isActivated
 
-    @field:JsonProperty("is_verified")
+    @get:JsonProperty("is_verified")
     val isVerified = account.isVerified
 
-    @field:JsonProperty("is_blocked")
+    @get:JsonProperty("is_blocked")
     val isBlocked = account.isBlocked
 
     @field:JsonProperty("permissions")
